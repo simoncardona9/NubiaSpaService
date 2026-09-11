148d6e2e889d696acaf1478a6dbb710787ae65f4
Simon Cardona <scardona9@gmail.com>
2026-09-11 11:30:01 -0300

    Mas cambios en analitis

---
+++ assets/google-consent.js
@@ -0,0 +1,12 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = function gtag() {
+  window.dataLayer.push(arguments);
+};
+
+window.gtag("consent", "default", {
+  ad_storage: "denied",
+  ad_user_data: "denied",
+  ad_personalization: "denied",
+  analytics_storage: "denied",
+});
+window.gtag("js", new Date());
